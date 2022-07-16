c208a3d96a5162a9bf6aa51dac71d2b343b1efab
Uzoma S. Uzoma <usumg1165@gmail.com>
2022-07-16 09:48:56 +0100

    Complete 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (typeof x != 'number' || typeof y != 'number' || x < 0 || y < 0) return 'ERROR';
+    
+    let smallNum = Math.min(x, y),
+        largeNum = Math.max(x, y);
+    
+    let sum = 0;
+    for (; smallNum <= largeNum; smallNum++) sum += smallNum;
 
+    return sum;
 };
 
 // Do not edit below this line
