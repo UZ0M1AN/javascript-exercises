ad7676051a424cfba4465771463c2c1b0189387f
Uzoma S. Uzoma <usumg1165@gmail.com>
2022-07-16 09:48:27 +0100

    Complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    const splicedArr = [...arr];
+    
+    for (const item of items) {
+        const itemIndex = splicedArr.indexOf(item);
+        if (itemIndex >= 0) splicedArr.splice(itemIndex, 1);
+    }
 
+    return splicedArr;
 };
 
 // Do not edit below this line
