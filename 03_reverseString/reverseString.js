d7140cf417c9b56696db1d55b490010c064f2326
Uzoma S. Uzoma <usumg1165@gmail.com>
2022-07-16 09:48:04 +0100

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversedStr = '';
 
+    for (const s of str) reversedStr = s + reversedStr;
+
+    return reversedStr;
 };
 
 // Do not edit below this line
